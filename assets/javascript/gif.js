d2183378950a9c754107bbc4a23818c28f067206
Danniel Zelaya <dannielzelaya@gmail.com>
2019-07-01 15:50:01 -0500

    created html frame

---
+++ assets/javascript/gif.js
@@ -0,0 +1,16 @@
+/* Pseudocode BrainStorming:
+
+**None of the buttons are written in the html file, but in the js instead**
+
+Create an array of strings with a related topic
+    - save this array as a variable called 'topics'
+    - Have each index show up as a preloaded button on the page
+If you click on a button, it'll give you 10 static gifs related to that button
+    - if gif is clicked on, it becomes animated and stops when clicked again
+    -every gif will have its rating underneath
+If you search and submit, a new button with the searched word will appear as an option
+Add a form to your page that takes a value from a user input box and adds it to your `topics` array. 
+Make a function call that takes each topic in the array and remakes the buttons on the page
+*/
+
+var topics = ["batman", "superman", "iron man", "wonder woman", "hulk", "thor", "spider man", "captain america", "black panther", "dr. strange", "star lord", "groot", "flash", "aquaman", "antman", "black widow", "vision", "scarlet witch"]
